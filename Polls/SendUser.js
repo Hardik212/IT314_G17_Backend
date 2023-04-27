326e71f045b7acb27dd1d2fdbb3ee8c4cbcd8716
Harshil Kapasi <202001141@daiict.ac.in>
2023-04-28 01:30:27 +0530

    allowing creator for results

--- Polls/SendUser.js
+++ Polls/SendUser.js
@@ -53,7 +53,7 @@ const SendpolltoUser = async (req, res) => {
     if(pollcreatorUserobj.followers[i] == currentUser){
       break;
     }
-    if(i == pollcreatorUserobj.followers.length-1){
+    if(i == pollcreatorUserobj.followers.length-1 && currentUser != pollcreatorUser){      // added for allowing creator to see result
       return res.status(403).send({
         error: "poll is private",
       });
